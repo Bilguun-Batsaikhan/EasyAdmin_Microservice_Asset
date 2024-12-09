24cb4facf2c47b5bcaf8ad7c43cd954eb2dd8a6e
DESKTOP-9D2J144\certimeter <bilguun.batsaikhan@lutech.it>
2024-12-09 10:18:47 +0100

    added pagination

--- src/main/java/com/example/table/controller/AssetController.java
+++ src/main/java/com/example/table/controller/AssetController.java
@@ -1,5 +1,6 @@
 package com.example.table.controller;
 
+import com.example.table.dto.AssetResPagination;
 import com.example.table.enumeration.HttpResponseEnum;
 import com.example.table.enumeration.UserRoleEnum;
 import com.example.table.exception.FailureException;
@@ -8,6 +9,8 @@ import com.example.table.requestcontext.RequestContext;
 import com.example.table.service.AssetService;
 import com.example.table.service.AuthorizationService;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.EnumSet;
@@ -31,38 +34,42 @@ public class AssetController {
     //CRUD operations for Asset //
     //--------------------------//
     @GetMapping
-    public List<Asset> getAllAssets() {
+    public ResponseEntity<AssetResPagination> getAllAssets(@RequestParam(value = "page", defaultValue = "0", required = false) int pageNo,
+                                                           @RequestParam(value = "pageSize", defaultValue = "10", required = false) int pageSize) {
         EnumSet<UserRoleEnum> authorizedRoles = EnumSet.of(UserRoleEnum.SUPER_ADMIN, UserRoleEnum.SYSTEM_ADMIN);
         if (!authorizationService.isAuthorized(requestContext, authorizedRoles)) {
             throw new FailureException(HttpResponseEnum.FORBIDDEN);
         }
-        return assetService.getAllAssets();
+        return new ResponseEntity<>(assetService.getAllAssets(pageNo, pageSize), HttpStatus.OK);
     }
 
     @PostMapping
-    public Asset createAsset(@Valid @RequestBody Asset asset) {
+    public ResponseEntity<Asset> createAsset(@Valid @RequestBody Asset asset) {
         EnumSet<UserRoleEnum> authorizedRoles = EnumSet.of(UserRoleEnum.SUPER_ADMIN, UserRoleEnum.SYSTEM_ADMIN);
         if (!authorizationService.isAuthorized(requestContext, authorizedRoles)) {
             throw new FailureException(HttpResponseEnum.FORBIDDEN);
         }
-        return assetService.createAsset(asset);
+        Asset createdAsset = assetService.createAsset(asset);
+        return new ResponseEntity<>(createdAsset, HttpStatus.CREATED);
     }
 
     @PatchMapping("/{id}")
-    public Asset updateAsset(@Valid @PathVariable Long id, @RequestBody Map<String, Object> updates) {
+    public ResponseEntity<Asset> updateAsset(@Valid @PathVariable Long id, @RequestBody Map<String, Object> updates) {
         EnumSet<UserRoleEnum> authorizedRoles = EnumSet.of(UserRoleEnum.SUPER_ADMIN, UserRoleEnum.SYSTEM_ADMIN);
         if (!authorizationService.isAuthorized(requestContext, authorizedRoles)) {
             throw new FailureException(HttpResponseEnum.FORBIDDEN);
         }
-        return assetService.updateAsset(id, updates);
+        Asset updatedAsset = assetService.updateAsset(id, updates);
+        return new ResponseEntity<>(updatedAsset, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public Asset removeAsset(@Valid @PathVariable Long id) {
+    public ResponseEntity<Asset> removeAsset(@Valid @PathVariable Long id) {
         EnumSet<UserRoleEnum> authorizedRoles = EnumSet.of(UserRoleEnum.SUPER_ADMIN, UserRoleEnum.SYSTEM_ADMIN);
         if (!authorizationService.isAuthorized(requestContext, authorizedRoles)) {
             throw new FailureException(HttpResponseEnum.FORBIDDEN);
         }
-        return assetService.removeAsset(id);
+        Asset removedAsset = assetService.removeAsset(id);
+        return new ResponseEntity<>(removedAsset, HttpStatus.OK);
     }
 }
