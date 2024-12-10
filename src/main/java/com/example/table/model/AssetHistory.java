c7f48717f176892b65b3deb4c5205d923399cdd5
DESKTOP-9D2J144\certimeter <bilguun.batsaikhan@lutech.it>
2024-12-10 12:42:34 +0100

    Added asset history, now it logs every operations on individual assets such as delete, insert, update, batch insert

---
+++ src/main/java/com/example/table/model/AssetHistory.java
@@ -0,0 +1,45 @@
+package com.example.table.model;
+
+import com.example.table.enumeration.AssetAction;
+import com.example.table.enumeration.AssetStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Table(name = "asset_history")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class AssetHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long assetId;
+
+    @Column(nullable = false)
+    private Long adminId; // ID of the admin making the change
+
+    private Long userId; // The user this asset is assigned to (if any)
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private AssetStatus status;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private AssetAction action;
+
+    @Column(nullable = false)
+    private LocalDateTime date;
+
+    private String comment;
+
+}
+
