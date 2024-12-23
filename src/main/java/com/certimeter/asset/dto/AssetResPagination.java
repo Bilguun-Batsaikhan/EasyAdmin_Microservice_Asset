e1cc97cd472ea37cc97f26e1c9f1d6584e9cae73
DESKTOP-9D2J144\certimeter <bilguun.batsaikhan@lutech.it>
2024-12-23 17:22:06 +0100

    added filters

--- src/main/java/com/certimeter/asset/dto/AssetResPagination.java
+++ src/main/java/com/certimeter/asset/dto/AssetResPagination.java
@@ -4,12 +4,69 @@ import com.certimeter.asset.model.Asset;
 import lombok.Data;
 
 import java.util.List;
+
 @Data
 public class AssetResPagination {
-    private List<Asset> data;
     private int pageNo;
     private int pageSize;
     private long totalElements;
     private int totalPages;
     private boolean last;
+    private List<Asset> data;
+
+    private AssetResPagination(Builder builder) {
+        this.pageNo = builder.pageNo;
+        this.pageSize = builder.pageSize;
+        this.totalElements = builder.totalElements;
+        this.totalPages = builder.totalPages;
+        this.last = builder.last;
+        this.data = builder.data;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private int pageNo;
+        private int pageSize;
+        private long totalElements;
+        private int totalPages;
+        private boolean last;
+        private List<Asset> data;
+
+        public Builder pageNo(int pageNo) {
+            this.pageNo = pageNo;
+            return this;
+        }
+
+        public Builder pageSize(int pageSize) {
+            this.pageSize = pageSize;
+            return this;
+        }
+
+        public Builder totalElements(long totalElements) {
+            this.totalElements = totalElements;
+            return this;
+        }
+
+        public Builder totalPages(int totalPages) {
+            this.totalPages = totalPages;
+            return this;
+        }
+
+        public Builder last(boolean last) {
+            this.last = last;
+            return this;
+        }
+
+        public Builder data(List<Asset> data) {
+            this.data = data;
+            return this;
+        }
+
+        public AssetResPagination build() {
+            return new AssetResPagination(this);
+        }
+    }
 }
